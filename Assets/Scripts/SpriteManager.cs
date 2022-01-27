5a892d875c07a7267693a3214f7ba04cb5dc6a97
sabs546 <55698170+sabs546@users.noreply.github.com>
2022-01-27 22:47:27 +0000

    More animations
    Jumping and falling

--- Assets/Scripts/SpriteManager.cs
+++ Assets/Scripts/SpriteManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SpriteManager : MonoBehaviour
 {
+    PlayerController controller;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller = GetComponent<PlayerController>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (controller.pState == PlayerController.PlayerStates.Grounded)
+        {
+            animator.SetInteger("YDir", 0);
+        }
+        else if (controller.pState == PlayerController.PlayerStates.Airborne)
+        {
+            if (controller.aState == PlayerController.AirStates.Rising)
+            {
+                animator.SetInteger("YDir", 1);
+            }
+            if (controller.aState == PlayerController.AirStates.Falling)
+            {
+                animator.SetInteger("YDir", -1);
+            }
+        }
     }
 }
