a0024c4f583f46c51395a6525f12da0eaf010358
sabs546 <55698170+sabs546@users.noreply.github.com>
2022-02-07 15:46:05 +0000

    Menu help section added

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject originalText;
+    public GameObject newText;
+    public GameObject menuBlock;
+    private RectTransform rect;
+    private Vector3 pos;
+    public float[] pageHeight;
+    public float scrollSpeed;
+    private bool changePage;
+    private int currentPage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rect = menuBlock.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (changePage)
+        {
+            pos = rect.anchoredPosition;
+            if (currentPage == 0)
+            {
+                if (pos.y < pageHeight[1])
+                {
+                    pos.y += scrollSpeed / WorldRules.physicsRate;
+                }
+                else
+                {
+                    pos.y = pageHeight[1];
+                    changePage = false;
+                    currentPage = 1;
+                }
+            }
+            else if (currentPage == 1)
+            {
+                if (pos.y > pageHeight[0])
+                {
+                    pos.y -= scrollSpeed / WorldRules.physicsRate;
+                }
+                else
+                {
+                    pos.y = pageHeight[0];
+                    changePage = false;
+                    currentPage = 0;
+                }
+            }
+            rect.anchoredPosition = pos;
+        }
+    }
+
+    public void ChangePage()
+    {
+        changePage = true;
+        if (currentPage == 0)
+        {
+            originalText.SetActive(false);
+            newText.SetActive(true);
+        }
+        if (currentPage == 1)
+        {
+            originalText.SetActive(true);
+            newText.SetActive(false);
+        }
+    }
+}
