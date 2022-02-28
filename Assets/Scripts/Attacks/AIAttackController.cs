00562d1f4d1c758ca2535284c79f673a270bdcc7
sabs546 <55698170+sabs546@users.noreply.github.com>
2022-02-28 23:49:07 +0000

    Planning for AI
    Mostly just dummy code

---
+++ Assets/Scripts/Attacks/AIAttackController.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIAttackController : MonoBehaviour
+{
+    // Attack Values =======================================================================================
+    public BaseAttack currentAttack;                              // Attack currently playing
+    private BaseAttack nextAttack;                                // The next available attack in the string
+    
+    public enum AttackState { Empty, Startup, Active, Recovery }; // Attack phases
+    public AttackState state;                                     // And what will hold them
+
+    // Player Values ========================================
+    private PlayerController controller;   // Player control
+    private SpriteRenderer sprite;         // Sprite control
+    private SetControls controls;          // Attack controls
+
+    // Attack Traits ====================================================
+    private PlayerPhysics physics;         // For your own knockback
+    private PlayerPhysics opponentPhysics; // For the opponents knockback
+    private BoxCollider2D hitbox;          // The hitbox of the attack
+    private int timer;                     // Frame counter
+    public  HitSparkManager hitSpark;      // The hit sparks
+    public int  stunLimit;                 // How long the stun lasts
+    public bool enableLowAttacks;          // Unlock crouch attacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<PlayerController>();
+        controls = GetComponent<SetControls>();
+        currentAttack = new BaseAttack();
+        nextAttack = new BaseAttack();
+        sprite = GetComponent<SpriteRenderer>();
+        state = AttackState.Empty;
+        timer = 0;
+        stunLimit = 0;
+
+        physics = GetComponent<PlayerPhysics>();
+        opponentPhysics = physics.opponent.GetComponent<PlayerPhysics>();
+        hitbox = gameObject.AddComponent<BoxCollider2D>();
+        hitbox.isTrigger = true;
+        hitbox.enabled = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // First attack
+        if (state == AttackState.Empty && controller.gState != PlayerController.GroundStates.Stun)
+        {
+            if (Input.GetKeyDown(controls.Punch))
+            {
+                currentAttack = FindAttack(controls.Punch);
+                if (currentAttack != null)
+                {
+                    if (sprite.flipX) { currentAttack.SideSwap(); }
+                    state = AttackState.Startup;
+                    nextAttack = currentAttack.Followup;
+                }
+            }
+            else if (Input.GetKeyDown(controls.Kick))
+            {
+                currentAttack = FindAttack(controls.Kick);
+                if (currentAttack != null)
+                {
+                    if (sprite.flipX) { currentAttack.SideSwap(); }
+                    state = AttackState.Startup;
+                    nextAttack = currentAttack.Followup;
+                }
+            }
+        }
+        // The next part of the string
+        else if (state == AttackState.Recovery)
+        {
+            if (nextAttack != null && ((Input.GetKeyDown(controls.Punch) && nextAttack.attackType == BaseAttack.AttackType.Punch) ||
+                                       (Input.GetKeyDown(controls.Kick)  && nextAttack.attackType == BaseAttack.AttackType.Kick)))
+            {
+                currentAttack = nextAttack;
+                GetComponent<SpriteManager>().EnableFollowup(true);
+                if (currentAttack != null)
+                {
+                    if (sprite.flipX) { currentAttack.SideSwap(); }
+                    state = AttackState.Startup;
+                    timer = 0;
+                    if (currentAttack.Followup != null)
+                    {
+                        nextAttack = currentAttack.Followup;
+                    }
+                }
+            }
+        }
+        // This one is an odd case, where an attack transitions into another attack automatically
+        else if (state == AttackState.Active)
+        {
+            if (currentAttack is FallingKick && controller.pState == PlayerController.PlayerStates.Grounded)
+            {
+                currentAttack = new SlideKick();
+                state = AttackState.Startup;
+                timer = 0;
+                currentAttack.RemoveRecoil();
+            }
+        }
+
+        // If there is some stun, start the clock
+        if (stunLimit > 0)
+        {
+            if (timer == 0)
+            {
+                controller.gState = PlayerController.GroundStates.Stun;
+                GetComponent<Animator>().SetBool("Stun", true);
+            }
+
+            if (timer < stunLimit)
+            {
+                timer++;
+            }
+            else
+            {
+                controller.gState = PlayerController.GroundStates.Neutral;
+                GetComponent<Animator>().SetBool("Stun", false);
+                stunLimit = 0;
+                timer = 0;
+            }
+        }
+
+        // What happens in the attack state
+        if (state != AttackState.Empty)
+        {
+            timer++;
+            if (timer < currentAttack.Speed.x) // During attack startup
+            {
+                state = AttackState.Startup;
+                if (currentAttack.AlwaysRecoil && !currentAttack.DelayRecoil)
+                {
+                    physics.travel += sprite.flipX ? currentAttack.Recoil.x / WorldRules.physicsRate : -currentAttack.Recoil.x / WorldRules.physicsRate;
+                    physics.launch += currentAttack.Recoil.y / WorldRules.physicsRate;
+                    currentAttack.RemoveRecoil();
+                }
+                if (timer == 1)
+                {
+                    if (currentAttack.SparkType == HitSparkManager.SparkType.Launch)
+                    {
+                        GetComponent<AttackAudioManager>().PlaySound("Whiff_Heavy_01");
+                    }
+                    else if (currentAttack.SparkType == HitSparkManager.SparkType.Mid || currentAttack.SparkType == HitSparkManager.SparkType.Low)
+                    {
+                        GetComponent<AttackAudioManager>().PlaySound("Whiff_Light_01");
+                    }
+                }
+            }
+            else if (timer < currentAttack.Speed.y) // During attack active
+            {
+                state = AttackState.Active;
+                hitbox.enabled = true;
+                hitbox.offset = currentAttack.Range;
+                hitbox.size = currentAttack.Size;
+                if (currentAttack.DelayRecoil)
+                {
+                    physics.travel += sprite.flipX ? currentAttack.Recoil.x / WorldRules.physicsRate : -currentAttack.Recoil.x / WorldRules.physicsRate;
+                    physics.launch += currentAttack.Recoil.y / WorldRules.physicsRate;
+                    currentAttack.RemoveRecoil();
+                }
+            }
+            else if (timer < currentAttack.Speed.z) // During attack recovery
+            {
+                state = AttackState.Recovery;
+                hitbox.enabled = false;
+                if (controller.gState == PlayerController.GroundStates.Sprint) physics.startSprint = false;
+            }
+            else // After attack completes
+            {
+                state = AttackState.Empty;
+                timer = 0;
+                stunLimit = 0;
+                GetComponent<SpriteManager>().EnableFollowup(false);
+            }
+        }
+    }
+
+    BaseAttack FindAttack(KeyCode attackType)
+    {
+        switch (controller.pState)
+        {
+            case PlayerController.PlayerStates.Grounded:
+                switch (controller.gState)
+                {
+                    case PlayerController.GroundStates.Neutral:
+                        break;
+
+                    case PlayerController.GroundStates.Dash:
+                        if (attackType == controls.Punch) { return new DashPunch(); }
+                        if (attackType == controls.Kick) { return new DashKick(); }
+                        break;
+
+                    case PlayerController.GroundStates.Sprint:
+                        if (attackType == controls.Punch) { return new SprintPunch(); }
+                        break;
+                }
+                break;
+
+            case PlayerController.PlayerStates.Airborne:
+                switch (controller.aState)
+                {
+                    case PlayerController.AirStates.Rising:
+                        if (attackType == controls.Punch) { return new RisingPunch(); }
+                        break;
+                    case PlayerController.AirStates.Falling:
+                        if (attackType == controls.Kick) { return new FallingKick(); }
+                        break;
+                }
+                break;
+
+            case PlayerController.PlayerStates.Crouching:
+                if (attackType == controls.Kick) { return new SlideKick(); }
+                break;
+        }
+        return null;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Weight stuff
+        physics.launch -= currentAttack.Recoil.y / WorldRules.physicsRate;
+        opponentPhysics.travel += currentAttack.Knockback.x / WorldRules.physicsRate;
+        opponentPhysics.launch += currentAttack.Knockback.y / WorldRules.physicsRate;
+        if (!currentAttack.AlwaysRecoil)
+        {
+            if (currentAttack.Recoil != Vector2.zero)
+            {
+                physics.travel -= currentAttack.Recoil.x / WorldRules.physicsRate;
+            }
+            else
+            {
+                physics.Brake();
+                opponentPhysics.Brake();
+            }
+        }
+        
+        // Hitspark stuff
+        if (opponentPhysics.GetComponent<PlayerController>().pState == PlayerController.PlayerStates.Grounded)
+        {
+            opponentPhysics.GetComponent<PlayerAttackController>().stunLimit = currentAttack.Stun;
+        }
+
+        Vector2 sparkPos = new Vector2(transform.position.x + (!sprite.flipX ? transform.lossyScale.x : -transform.lossyScale.x), transform.position.y);
+        if (currentAttack.SparkType == HitSparkManager.SparkType.Launch)
+        {
+            sparkPos.y += transform.lossyScale.y * 0.5f;
+        }
+        hitSpark.CreateHitSpark(currentAttack.SparkType, sparkPos.x, sparkPos.y, !sprite.flipX, physics.travel, controller.pState);
+
+        // Other stuff
+        opponentPhysics.GetComponent<HealthManager>().SendDamage(currentAttack.Damage);
+        GetComponent<AttackAudioManager>().PlaySound(currentAttack.SoundName);
+        timer = currentAttack.Speed.y;
+    }
+}
---
+++ Assets/Scripts/Movement/AIController.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIController : MonoBehaviour
+{
+    public float sprint;
+    public float dashDistance;
+    public float jumpPower;
+
+    public enum PlayerStates { Crouching, Grounded, Airborne };
+    public enum GroundStates { Neutral, Dash, Backdash, Sprint, Stun };
+    public enum AirStates    { Rising, Falling };
+    public PlayerStates pState;
+    public GroundStates gState;
+    public AirStates    aState;
+    public enum Playstyle { Rushdown, Adaptive, Turtle }; // The AI needs a decision making process, but they need something to decide why they would make a decision
+    public Playstyle playStyle;                           // Rather than just always being optimal, the playstyle should decide what they want to do at any given time
+
+    public enum Side { Left, Right };
+    public Side currentSide;
+
+    public GameObject opponent;
+
+    private SetControls controls;
+    private PlayerPhysics physics;
+    private AIAttackController attackController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controls = GetComponent<SetControls>();
+        physics = GetComponent<PlayerPhysics>();
+        attackController = GetComponent<AIAttackController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // --------------------------------------------------------
+        // - Movement Inputs -
+        // -------
+        // You shouldn't be able to move while attacking
+        if (attackController.state != AIAttackController.AttackState.Empty || gState == GroundStates.Stun)
+        {
+            if (attackController.state == AIAttackController.AttackState.Startup)
+            {
+                
+            }
+            else if (attackController.state == AIAttackController.AttackState.Active)
+            {
+                
+            }
+            else if (attackController.state == AIAttackController.AttackState.Recovery)
+            {
+
+            }
+
+            // Sprint attacks need to still cancel
+            if ((Input.GetKeyUp(controls.Left) || Input.GetKeyUp(controls.Right)) && gState != GroundStates.Neutral)
+            {
+                physics.startSprint = false;
+                physics.travel = 0.0f;
+            }
+        }
+        else if (pState == PlayerStates.Grounded && gState != GroundStates.Stun)
+        {
+            if (Input.GetKeyDown(controls.Up))
+            {
+                physics.launch += jumpPower;
+            }
+
+            if (Input.GetKeyDown(controls.Down))
+            {
+                if (gState == GroundStates.Dash)
+                {
+                    physics.enableCrouch = true;
+                }
+            }
+
+            if (Input.GetKeyDown(controls.Left) && gState == GroundStates.Neutral)
+            {
+                physics.travel -= dashDistance;
+            }
+            else if (Input.GetKeyDown(controls.Left) && gState != GroundStates.Neutral)
+            {
+                physics.startSprint = true;
+                physics.travel = -sprint;
+            }
+            else if (Input.GetKeyUp(controls.Left) && gState != GroundStates.Neutral)
+            {
+                physics.startSprint = false;
+                physics.travel = 0.0f;
+            }
+
+            if (Input.GetKeyDown(controls.Right) && gState == GroundStates.Neutral)
+            {
+                physics.travel += dashDistance;
+            }
+            else if (Input.GetKeyDown(controls.Right) && gState != GroundStates.Neutral)
+            {
+                physics.startSprint = true;
+                physics.travel = sprint;
+            }
+            else if (Input.GetKeyUp(controls.Right) && gState != GroundStates.Neutral)
+            {
+                physics.startSprint = false;
+                physics.travel = 0.0f;
+            }
+
+            // For missed input ups
+            if (gState == GroundStates.Sprint && !Input.GetKey(controls.Left) && !Input.GetKey(controls.Right))
+            {
+                physics.startSprint = false;
+            }
+        }
+        else if (pState == PlayerStates.Airborne)
+        {
+            if (gState == GroundStates.Sprint)
+            {
+                if (Input.GetKeyDown(controls.Left) && physics.airLock == -1)
+                {
+                    physics.startSprint = true;
+                    physics.travel = -sprint;
+                }
+                else if (Input.GetKeyDown(controls.Right) && physics.airLock == 1)
+                {
+                    physics.startSprint = true;
+                    physics.travel = sprint;
+                }
+
+                if (Input.GetKeyUp(controls.Left) || Input.GetKeyUp(controls.Right))
+                {
+                    physics.startSprint = false;
+                    physics.travel = 0.0f;
+                }
+            }
+        }
+    }
+
+    private void DecisionMaker()
+    {
+        PlayerController opponentController = opponent.GetComponent<PlayerController>();
+        PlayerAttackController opponentAtkController = opponent.GetComponent<PlayerAttackController>();
+        bool attackRange = false; // This will tell you if you're in attack range
+
+        if (playStyle == Playstyle.Rushdown)
+        {
+            // attackRange = CheckIfInAttackRange();
+            // Regardless of attack range, we're going to need some sort of movement
+            // Being rushdown, movement will be approach, so we're going to assume we dash here
+            if (!attackRange)
+            {
+                // The important part though is that we're not within attack range, so below is what we do from that dash
+                // Being aggressive, we've decided already that they're going to approach
+                // The question is how?
+                // To determine this we're gonna need to know the current status of the opponent, for example grounded
+                if (opponentController.pState == PlayerController.PlayerStates.Grounded && pState == PlayerStates.Grounded)
+                {
+                    // The following are dummy variables for the if statement below
+                    int distance = 0;  // So first we find their range from us
+                    int threshold = 0; // This will determine if they want to swap to sprint or not
+
+                    // We've aready started our approach, lets pretend they are too far away and we need to sprint instead
+                    if (distance > threshold)
+                    {
+                        // This is where we'd send the signal to start the sprint
+                    }
+                    // Otherwise continue with the dash decision Until we're within attack range
+                }
+            }
+            else
+            {
+                // We're within range, we should probably throw out some kinda attack
+                // Lets deal with that in another script at another time, we're just focusing on movement right now
+            }
+        }
+    }
+}
