eb8c44a25f2c77275cce153a88267a7ef6d225db
sabs546 <55698170+sabs546@users.noreply.github.com>
2022-07-16 00:23:21 +0100

    Healthbar resizing
    Makes it easier to see on higher resolution screens

---
+++ Assets/Scripts/UI/ChangeScale.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScale : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform objectToScale;
+    private Vector2 originalScale;
+
+    private void Start()
+    {
+        originalScale = objectToScale.localScale;
+    }
+
+    public void Rescale(float amount)
+    {
+        objectToScale.localScale *= new Vector2(amount, amount);
+    }
+
+    public void Revert()
+    {
+        objectToScale.localScale = originalScale;
+    }
+}
