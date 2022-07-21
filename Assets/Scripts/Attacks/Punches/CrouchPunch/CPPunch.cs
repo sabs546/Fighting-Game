cab24f1ed912a2a920ac305feb55f51e62ecf419
sabs546 <55698170+sabs546@users.noreply.github.com>
2022-07-21 18:44:42 +0100

    New attack
    Crouch uppercut now has a punch followup
    Updated command list layout

--- Assets/Scripts/Attacks/Punches/CrouchPunch/CPPunch.cs
+++ Assets/Scripts/Attacks/Punches/CrouchPunch/CPPunch.cs
@@ -2,29 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrouchPunch : BaseAttack
+public class CPPunch : BaseAttack
 {
-    public CrouchPunch()
+    public CPPunch()
     {
         attackType = AttackType.Punch;
-        Damage = 15;
-        Speed = new Vector3Int(3, 5, 27);
-        Stun = 30;
-        Followup = new CPPunch();
+        Damage = 5;
+        Speed = new Vector3Int(15, 17, 37);
+        Stun = 10;
+        Followup = null;
 
-        Range = new Vector2(0.5f, 0.0f);
+        Range = new Vector2(0.6f, 0.0f);
         Size = new Vector2(0.5f, 1.0f);
         Recoil = new Vector2(0.0f, 0.0f);
-        Knockback = new Vector2(0.0f, 16.0f);
+        Knockback = new Vector2(32.0f, -16.0f);
         AlwaysRecoil = false;
         DelayRecoil = false;
-        SparkType = HitSparkManager.SparkType.Launch;
+        SparkType = HitSparkManager.SparkType.Mid;
         SoundName = "Heavy_01";
     }
 
     public override void SideSwap()
     {
-        Range = new Vector2(-Range.x, Range.y);
+        Range = new Vector2(-Range.x, -Range.y);
         Recoil = new Vector2(-Recoil.x, Recoil.y);
         Knockback = new Vector2(-Knockback.x, Knockback.y);
     }
